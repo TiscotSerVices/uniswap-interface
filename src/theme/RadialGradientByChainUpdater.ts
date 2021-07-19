8cd4191fe862d39f7ae753effdc947d4f05fe678
TiscotSerVices <tiscot2019@gmail.com>
2021-07-20 00:00:24 +0200

    color update 2

--- src/theme/RadialGradientByChainUpdater.ts
+++ src/theme/RadialGradientByChainUpdater.ts
@@ -36,14 +36,14 @@ export default function RadialGradientByChainUpdater(): null {
       case SupportedChainId.ARBITRUM_ONE:
       case SupportedChainId.ARBITRUM_RINKEBY:
         setBackground(backgroundResetStyles)
-        const arbitrumLightGradient = 'radial-gradient(150% 100% at 50% 0%, #CDE8FB 0%, #FCF3F9 50%, #FFFFFF 100%)'
-        const arbitrumDarkGradient = 'radial-gradient(150% 100% at 50% 0%, #0A294B 0%, #221E30 50%, #1F2128 100%)'
+        const arbitrumLightGradient = 'radial-gradient(150% 100% at 50% 0%, #FF7900 0%, #FFD600 50%, #FFFF00 100%)'
+        const arbitrumDarkGradient = 'radial-gradient(150% 100% at 50% 0%, #FF7900 0%, #FFD600 50%, #FFFF00 100%)'
         backgroundRadialGradientElement.style.background = darkMode ? arbitrumDarkGradient : arbitrumLightGradient
         break
       case SupportedChainId.OPTIMISM:
       case SupportedChainId.OPTIMISTIC_KOVAN:
         setBackground(backgroundResetStyles)
-        const optimismLightGradient = 'radial-gradient(150% 100% at 50% 0%, #FFFBF2 2%, #FFF4F9 53%, #FFFFFF 100%)'
+        const optimismLightGradient = 'radial-gradient(150% 100% at 50% 0%, #FF7900 2%, #FFD600 53%, #FFFF00 100%)'
         const optimismDarkGradient = 'radial-gradient(150% 100% at 50% 0%, #FF7900 2%, #FFD600 53%, #FFFF00 100%)'
         backgroundRadialGradientElement.style.background = darkMode ? optimismDarkGradient : optimismLightGradient
         break
